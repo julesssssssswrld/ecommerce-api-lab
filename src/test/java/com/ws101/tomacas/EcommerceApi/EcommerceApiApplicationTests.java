77353734fceeea9937687161b6f9b278a1f013b2
Ian <tomacasjulesiancajandab@gmail.com>
2026-04-29 20:20:32 +0800

    Revert "fix: added H2 dependency and test profile for offline build verification"
    This reverts commit 5685d3e7ee27a8530238739e3a1dec8a483dca94.

--- src/test/java/com/ws101/tomacas/EcommerceApi/EcommerceApiApplicationTests.java
+++ src/test/java/com/ws101/tomacas/EcommerceApi/EcommerceApiApplicationTests.java
@@ -3,10 +3,7 @@ package com.ws101.tomacas.EcommerceApi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.context.ActiveProfiles;
-
 @SpringBootTest
-@ActiveProfiles("test")
 class EcommerceApiApplicationTests {
 
 	@Test
